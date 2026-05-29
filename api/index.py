449cfe121a9a0ec695ed859508c35c0645d648a5
Matthew Hernandez <matt13tx@yahoo.com>
2026-05-29 12:30:42 -0700

    Fix CORS config

--- api/index.py
+++ api/index.py
@@ -12,7 +12,12 @@ app = FastAPI()
 # CORS so the frontend can talk to backend
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+     allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://frontend-ihkdmurfr-matthew-ai.vercel.app",
+    ],
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"]
 )
