454b5f418922d7f4c09c5ea5416bc5b1e2499cd4
Matthew Hernandez <matt13tx@yahoo.com>
2026-05-29 12:36:45 -0700

    Allow latest frontend origin

--- api/index.py
+++ api/index.py
@@ -15,7 +15,8 @@ app.add_middleware(
      allow_origins=[
         "http://localhost:3000",
         "http://127.0.0.1:3000",
-        "https://frontend-ihkdmurfr-matthew-ai.vercel.app",
+        "https://frontend-ihkdmurfr-matthew-ai.vercel.app"
+        "https://frontend-57xmx8ln3-matthew-ai.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
